8152b6f18658e13d11ac5eb29562b40c8429dd38
Hiromi Cota <hiromi.cota@gmail.com>
2017-06-01 10:18:44 -0700

    Working on parse

--- Project4/Project4/parse.h
+++ Project4/Project4/parse.h
@@ -1,3 +1,4 @@
+
 #ifndef PARSE_H
 #define PARSE_H
 
@@ -13,12 +14,12 @@ namespace PB_CALC
 	{
 		//Takes m_instrStream istream and calls appropriate function with correct arguments
 		double leftTerm, rightTerm;
+		string token;
 		m_instrStream >> leftTerm;	//Grab first number and consume
 		m_instrStream >> rightTerm;	//Grab second number and consume
-		while (isdigit(m_instrStream.peek()) != 1)	//Seek stream for 
-		{
+		m_instrStream >> token;
+		if (token == "+")
 
-		}
 		
 		
 	}
